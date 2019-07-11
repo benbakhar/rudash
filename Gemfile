076c036d49e2127f3ec14f64f583d78927352450
Islam Attrash <isattrash@gmail.com>
2019-07-11 17:49:04 +0300

    fix bundler

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'power_assert', "=1.1.4"
 gem 'test-unit'
--- test-runner.rb
+++ test-runner.rb
@@ -1,3 +1,19 @@
-for file in Dir['./test/*.rb']
-    system('ruby ' + file)
+# require 'bundler/setup'
+
+# for file in Dir['./test/*.rb']
+#     system('ruby ' + file)
+# end
+
+require 'bundler/setup'
+require 'rudash'
+require 'test/unit'
+
+class IsNilTest < Test::Unit::TestCase
+    def test_nil
+        assert_equal R_.isNil[nil], true
+    end
+
+    def test_not_nil
+        assert_equal R_.isNil[0], false
+    end
 end

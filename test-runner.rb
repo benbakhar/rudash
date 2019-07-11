f32f5b73369f4e96e8436f0415b4b9372b38e338
Islam Attrash <isattrash@gmail.com>
2019-07-11 17:52:45 +0300

    Revert "fix bundler"
    This reverts commit 076c036d49e2127f3ec14f64f583d78927352450.

--- test-runner.rb
+++ test-runner.rb
@@ -1,19 +1,3 @@
-# require 'bundler/setup'
-
-# for file in Dir['./test/*.rb']
-#     system('ruby ' + file)
-# end
-
-require 'bundler/setup'
-require 'rudash'
-require 'test/unit'
-
-class IsNilTest < Test::Unit::TestCase
-    def test_nil
-        assert_equal R_.isNil[nil], true
-    end
-
-    def test_not_nil
-        assert_equal R_.isNil[0], false
-    end
+for file in Dir['./test/*.rb']
+    system('ruby ' + file)
 end

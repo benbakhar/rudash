282096007dfb4f0d5c52a33190ee5c7e08e35780
Islam Attrash <isattrash@gmail.com>
2019-07-14 09:37:04 +0300

    [FEAT] Add arity check to R_.map to support more dynamic usage

--- lib/map.rb
+++ lib/map.rb
@@ -1,7 +1,9 @@
 module Map
     def map
-        map_proc = -> (array, mapperFn) {
-            array.map { |value| mapperFn[value] }
+        map_proc = -> (array, mapper_proc) {
+            mapper_proc.arity == 1 ?
+                array.map { |value| mapper_proc[value] } :
+                array.map.with_index { |value, index| mapper_proc[value, index] }
         }
     end
 end
--- test/map.rb
+++ test/map.rb
@@ -7,4 +7,12 @@ class MapTest < Test::Unit::TestCase
         result = R_.map[[1,2,3], double]
         assert_equal result, [2,4,6]
     end
+
+    def test_double_even_indexes
+        double_even_index = -> (value, index) {
+            index % 2 === 0 ? value * 2 : value
+        }
+        result = R_.map[[1,2,3,4,5,6,7,8,9], double_even_index]
+        assert_equal result, [2,2,6,4,10,6,14,8,18]
+    end
 end

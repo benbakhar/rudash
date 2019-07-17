8b8f748a8695e6dfecc6fc783d71e77042dabb5e
Islam Attrash <isattrash@gmail.com>
2019-07-17 23:36:26 +0300

    break tests

--- test/compact.rb
+++ test/compact.rb
@@ -9,6 +9,6 @@ class CompactTest < Test::Unit::TestCase
 
     def test_with_nils
         result = R_.compact[[1, 0, nil, "islam"]]
-        assert_equal result, [1, 0, "islam"]
+        assert_equal result, [1, 0, "islamx"]
     end
 end

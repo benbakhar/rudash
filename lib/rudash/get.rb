6e40046858fd862a1fab95bc5f5519d5c707aedc
Islam Attrash <isattrash@gmail.com>
2019-07-19 19:28:27 +0300

    [FIX] R_.get string hash keys. Resolves #10

--- lib/rudash/get.rb
+++ lib/rudash/get.rb
@@ -1,7 +1,9 @@
 require_relative 'reduce.rb'
+require_relative 'filter.rb'
 
 module Get
     extend Reduce
+    extend Filter
 
     def get
         get_proc = -> (hash, path, *rest_args) {
@@ -15,13 +17,18 @@ module Get
                   normalized_path = normalized_path.join('.') 
                 end
 
-                normalized_path = normalized_path.gsub /[\[\]]/, "."
-                normalized_path = normalized_path.gsub /\.\./, "."
-                splitted_hash = normalized_path.split('.')
+                filter_not_empty = -> (value) {
+                    value != ''
+                }
+
+                splitted_hash = self.filter[
+                    normalized_path.split(/[.\[\]]/),
+                    filter_not_empty
+                ]
             }
 
             splitted_hash = transform_path[path]
-
+            
             get_reducer = -> (acc, current) {
                 return nil if acc.nil?
 
@@ -30,7 +37,7 @@ module Get
                 elsif acc.is_a?(Array) and !current.match(/^(\d)+$/)
                     nil
                 elsif acc.is_a?(Hash)
-                    acc[current.to_sym]
+                    acc[current.to_sym] || acc[current]
                 else
                     nil
                 end
--- test/get.rb
+++ test/get.rb
@@ -2,16 +2,24 @@ require_relative '../lib/rudash'
 require 'test/unit'
 
 class GetTest < Test::Unit::TestCase
-    def test_hash
+    def test_sym_hash
         hash = { a: 1, b: 2 }
         assert_equal R_.get[hash, 'a'], 1
         assert_equal R_.get[hash, 'b'], 2
         assert_equal R_.get[hash, 'b.sc.cd.cd.d'], nil
     end
 
+    def test_string_hash
+        hash = { 'a' => 1, 'b' => 2 }
+        assert_equal R_.get[hash, 'a'], 1
+        assert_equal R_.get[hash, 'b'], 2
+    end
+
     def test_array
         array = [1,2,3]
         assert_equal R_.get[array, '0'], 1
+        array = [{'a' => 55}]
+        assert_equal R_.get[array, '[0].a'], 55
     end
 
     def test_deep_path_in_hash

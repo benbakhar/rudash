a637b44e1d3a9b69864efd1de9a7cf92dc9735cb
Islam Attrash <isattrash@gmail.com>
2019-07-28 19:36:33 +0300

    [FIX] Fix negate to work with Method and Proc. Resolves #56

--- lib/rudash/reject.rb
+++ lib/rudash/reject.rb
@@ -3,12 +3,12 @@ require_relative '../utils/subset_deep_match.rb'
 module Rudash
     module Reject
         def reject(collection, *rest_args)
-            filter = self.head(rest_args) || self.method(:identity).to_proc
+            filter = self.head(rest_args) || self.method(:identity)
 
             if filter.is_a?(Hash)
                 slice_matcher = Rudash::SubsetDeepMatch.subset_deep_match?.(filter)
                 return self.filter(collection, self.negate(slice_matcher)) 
-            elsif filter.is_a?(Proc)
+            elsif filter.is_a?(Proc) || filter.is_a?(Method)
                 return self.filter(collection, self.negate(filter))
             else
                 return []

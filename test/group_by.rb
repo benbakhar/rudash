8a1b0bff3b7b29b56ed9100c999714653e86b41b
Islam Attrash <isattrash@gmail.com>
2019-08-15 14:29:03 +0300

    More test cases

--- test/group_by.rb
+++ test/group_by.rb
@@ -4,16 +4,31 @@ require 'test/unit'
 class GroupByTest < Test::Unit::TestCase
     def test_without_iterator_fn
         collection = [6.1, 4.2, 6.3]
-        expected = {6.1=> [6.1], 4.2=> [4.2], 6.3=> [6.3]}
+        expected = { 6.1 => [6.1], 4.2 => [4.2], 6.3 => [6.3] }
 
         assert_equal R_.group_by(collection), expected
     end
 
-    def with_iterator_fn
+    def test_with_iterator_fn
         collection = [6.1, 4.2, 6.3]
         iterator = ->(value) { value.floor }
         expected = { 4 => [4.2], 6 => [6.1, 6.3] }
 
         assert_equal R_.group_by(collection, iterator), expected
+    end
+
+    def test_hash
+        hash = { a: 6.1, b: 4.2, c: 6.3 }
+        expected = { 6.1 => [6.1], 4.2 => [4.2], 6.3 => [6.3] }
+        
+        assert_equal R_.group_by(hash), expected
+    end
+
+    def test_hash_with_iterator
+        hash = { a: 6.1, b: 4.2, c: 6.3 }
+        iterator = ->(value) { value.floor }
+        expected = { 4 => [4.2], 6 => [6.1, 6.3] }
+
+        assert_equal R_.group_by(hash, iterator), expected
     end
 end

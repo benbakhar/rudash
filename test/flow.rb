0545de2f4a1aef36354624e946dcbecae26fd4f1
Islam Attrash <isattrash@gmail.com>
2019-10-15 11:11:44 +0300

    complex test case

--- test/flow.rb
+++ test/flow.rb
@@ -17,4 +17,21 @@ class FlowTest < Test::Unit::TestCase
         composed = R_.flow([inc_two_numbers], square, double)
         assert_equal 1800, composed.(10, 20)
     end
+
+    def test_hash
+        hash = { a: 1 }
+        set_path = R_.curry(-> (path, value, _) {
+            p "updating #{path} with #{value} in hash"
+            R_.update(hash, path, -> () { value })
+        })
+
+        composed = R_.flow([
+            set_path.('a', 5),
+            set_path.('b', { b: 1 }),
+            set_path.('c', 'string')
+        ])
+
+        result = composed.()
+        p result
+    end
 end
